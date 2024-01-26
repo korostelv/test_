13dc41eb66e5444fee1ddc81d73f3886590d65b1
korostelv <valenok2025@gmail.com>
2024-01-26 14:20:43 +0500

    add model

--- payment/info_pay/admin.py
+++ payment/info_pay/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Requisit, Application
 
-# Register your models here.
+
+class RequisitAdmin(admin.ModelAdmin):
+    model = Requisit
+    list_display = (
+        'id', 'type_payment', 'last_name', 'first_name', 'patronymic', 'phone', 'limit', 'card_type', 'account_type')
+    list_filter = ('type_payment',)
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    model = Application
+    list_display = ('id', 'summ', 'status', 'requisit')
+    list_filter = ('summ', 'status', 'requisit',)
+
+
+admin.site.register(Requisit, RequisitAdmin)
+admin.site.register(Application, ApplicationAdmin)
--- payment/info_pay/models.py
+++ payment/info_pay/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Requisit(models.Model):
+    Choices_pay = [
+        ('Card', "Карта"),
+        ('Payment account', "Платежный счет")
+    ]
+    Choices_card = [
+        ('Debit', 'Дебетовая'),
+        ('Credit', 'Кредитная'),
+        ('Prepaid', 'Предоплаченная')
+    ]
+    Choices_account = [
+        ('Current', 'Текущий'),
+        ('Saving', 'Сберегательный'),
+        ('Business', 'Бизнес-счет')
+    ]
+    type_payment = models.CharField(max_length=20, choices=Choices_pay, default='Card')
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=12)
+    limit = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    card_type = models.CharField(choices=Choices_card, max_length=20, blank=True, null=True)
+    account_type = models.CharField(choices=Choices_account, max_length=20, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.type_payment == 'Card':
+            self.account_type = None
+        elif self.type_payment == 'Payment account':
+            self.card_type = None
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
+
+class Application(models.Model):
+    Choices_status = [
+        ('Awaiting payment', 'Ожидает оплаты'),
+        ('Paid', 'Оплачена'),
+        ('Canceled', 'Отменена')
+    ]
+    summ = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    status = models.CharField(choices=Choices_status, max_length=20)
+    requisit = models.ForeignKey('Requisit',blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.summ}'
+
+
