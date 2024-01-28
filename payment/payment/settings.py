52d57ef0fa3e0d550630c78984944bf965dd1076
korostelv <valenok2025@gmail.com>
2024-01-28 20:35:03 +0500

    add bootstrap

--- payment/payment/settings.py
+++ payment/payment/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'info_pay'
+    'info_pay',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
