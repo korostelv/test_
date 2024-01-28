832833803a3312675c9ae4c2d3280683ce594472
korostelv <valenok2025@gmail.com>
2024-01-28 15:38:51 +0500

    add appl

--- payment/info_pay/views.py
+++ payment/info_pay/views.py
@@ -7,7 +7,7 @@ from .models import Application, Requisit
 class ApplicationListView(ListView):
     model = Application
     paginate_by = 100
-    template_name = 'index.html'
+    template_name = 'application.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
