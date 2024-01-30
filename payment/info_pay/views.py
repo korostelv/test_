3419b8f766e9f4beba5fddea73d3a7ba59a39681
korostelv <valenok2025@gmail.com>
2024-01-30 13:40:35 +0500

    add pagination

--- payment/info_pay/views.py
+++ payment/info_pay/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-# from .forms import ApplicationForm, RequisitForm
 from .models import Application, Requisit
 
 
