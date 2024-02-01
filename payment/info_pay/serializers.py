105a68bbe1b60e760b3e17206473ab03955df09d
korostelv <valenok2025@gmail.com>
2024-02-01 20:50:09 +0500

    update models

---
+++ payment/info_pay/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Application, Requisit
+
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = '__all__'
+
+
+class RequisitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Requisit
+        fields = '__all__'
